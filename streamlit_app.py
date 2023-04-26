ca2af448860529fe921e512557e9b2c3f33a1f7d
Fahim Muntasir <niloyf3@gmail.com>
2023-04-26 23:30:39 +0600

    bio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.write("Is this working?")
+st.header("Fahim Muntasir")
+st.write("A fresh graduate with interest in machine learning and data science. I have knowledge in interpreting and analyzing various types of statistical figures. Currently working on more robust models using deep learning. My diverse portfolio and skill set would surely speak to the fact that I am a quick learner and a relentless enthusiast.")
